b5116597d12ac8c8a1c799ade13e39997b3417a1
ANANYAQA-creator <ananyrawte@gmail.com>
2026-05-04 01:49:05 +0530

    Project Update1

--- src/test/java/Login_Register.java
+++ src/test/java/Login_Register.java
@@ -3,22 +3,22 @@ import org.testng.annotations.Test;
 public class Login_Register {
 
     @Test
-    public void register() {
+    public void tc1_register() {
         System.out.println("Register Successfully");
     }
 
     @Test
-    public void login() {
-        System.out.print("Login Successfully");
+    public void tc2_login() {
+        System.out.println("Login Successfully");
     }
 
     @Test
-    public void exit() {
+    public void tc3_exit() {
         System.out.println("Exit successfully");
     }
 
     @Test
-    public void function() {
+    public void tc4_function() {
         System.out.println("function added ");
     }
 }
